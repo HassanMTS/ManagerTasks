5c819a2caa9fe70752acc2dd6c2db73fa61d8b20
HassanMTS <150341521+HassanMTS@users.noreply.github.com>
2023-12-21 20:45:38 +0000

    Task Page version 1.0

---
+++ app/Components/Tasks/Tasks.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { useGlobalState } from "@/app/context/globalProvider";
+import React from "react";
+import styled from "styled-components";
+
+function Tasks() {
+    const { theme } = useGlobalState();
+    return <TaskStyled>Tasks</TaskStyled>;
+}
+
+const TaskStyled = styled.div`
+    padding: 2rem;
+    width: 100%;
+    background-color: ${(props) => props.theme.colorBg2};
+    border: 2px solid ${(props) => props.theme.borderColor2};
+    border-radius: 1rem;
+
+
+    overflow-y auto;
+`;
+
+export default Tasks;
