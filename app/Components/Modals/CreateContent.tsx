68577f86da3a6b29215f74cf92aebd2997691370
HassanMTS <150341521+HassanMTS@users.noreply.github.com>
2023-12-27 13:17:49 +0000

    Create task implemented

--- app/Components/Modals/CreateContent.tsx
+++ app/Components/Modals/CreateContent.tsx
@@ -1,38 +1,58 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function CreateContent() {
-        const [title, setTitle] = useState("");
-        const [description, setDescription] = useState("");
-        const [date, setDate] = useState("");
-        const [completed, setCompleted] = useState(false);
-        const [important, setImportant] = useState(false);
-        return (
-        <div>
-        <h1>Create a Task</h1>
-        <div className="input-control">
-                <label htmlFor="title">Title</label>
-                <input type="text"
-                id="title"
-                value={title}
-                onChange={handleChange("title")}
-                placeholder="e.g, Watch a video"
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [completed, setCompleted] = useState(false);
+  const [important, setImportant] = useState(false);
 
-                />
-        </div>
-        <div className="input-control">
+  function handleChange(field: string): React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> {
+    return (event) => {
+      const value = event.target.value;
+
+      switch (field) {
+        case "title":
+          setTitle(value);
+          break;
+        case "description":
+          setDescription(value);
+          break;
+        // Add cases for other fields if needed
+        default:
+          break;
+      }
+    };
+  }
+
+  return (
+    <div>
+      <h1>Create a Task</h1>
+      <div className="input-control">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={handleChange("title")}
+          placeholder="e.g, Watch a video"
+        />
+      </div>
+      <div className="input-control">
         <label htmlFor="description">Description</label>
         <textarea
-                value={description}
-                onChange={handleChange("description")}
-                name="description"
-                id="description"
-                rows={4}
-                placeholder="e.g, Watch a video about Next.js Auth"
+          value={description}
+          onChange={handleChange("description")}
+          name="description"
+          id="description"
+          rows={4}
+          placeholder="e.g, Watch a video about Next.js Auth"
         ></textarea>
-        </div>
-        </div>
-        );
+      </div>
+      {/* Add other input fields as needed */}
+    </div>
+  );
 }
 
 export default CreateContent;
