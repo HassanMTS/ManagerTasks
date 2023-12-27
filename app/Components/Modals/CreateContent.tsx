50b0eb1c515cfa7740e8f619e5216b8de854e05e
HassanMTS <150341521+HassanMTS@users.noreply.github.com>
2023-12-27 16:13:55 +0000

    Create task version 1.2

--- app/Components/Modals/CreateContent.tsx
+++ app/Components/Modals/CreateContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import Button from "../Button/Button";
 
 function CreateContent() {
   const [title, setTitle] = useState("");
@@ -8,23 +9,32 @@ function CreateContent() {
   const [completed, setCompleted] = useState(false);
   const [important, setImportant] = useState(false);
 
-  function handleChange(field: string): React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> {
-    return (event) => {
-      const value = event.target.value;
+  const handleChange = (name: string) => (e: any) => {
+    switch (name) {
+      case "title":
+        setTitle(e.target.value);
+        break;
+      case "description":
+        setDescription(e.target.value);
+        break;
+      case "date":
+        setDate(e.target.value);
+        break;
+      case "completed":
+        setCompleted(e.target.checked);
+        break;
+      case "important":
+        setImportant(e.target.checked);
+        break;
+      default:
+        break;
+    }
+  };
 
-      switch (field) {
-        case "title":
-          setTitle(value);
-          break;
-        case "description":
-          setDescription(value);
-          break;
-        // Add cases for other fields if needed
-        default:
-          break;
-      }
-    };
-  }
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Add your form submission logic here
+  };
 
   return (
     <div>
@@ -35,8 +45,9 @@ function CreateContent() {
           type="text"
           id="title"
           value={title}
+          name="title"
           onChange={handleChange("title")}
-          placeholder="e.g, Watch a video"
+          placeholder="e.g, Watch a video from Fireship."
         />
       </div>
       <div className="input-control">
@@ -47,10 +58,39 @@ function CreateContent() {
           name="description"
           id="description"
           rows={4}
-          placeholder="e.g, Hello this is made by Hassan Talib"
+          placeholder="e.g, Watch a video about Next.js Auth"
         ></textarea>
       </div>
-      {/* Add other input fields as needed */}
+      <div className="input-control">
+        <label htmlFor="date">Date</label>
+        <input
+          value={date}
+          onChange={handleChange("date")}
+          type="date"
+          name="date"
+          id="date"
+        />
+      </div>
+      <div className="input-control toggler">
+        <label htmlFor="completed">Toggle Completed</label>
+        <input
+          value={completed.toString()}
+          onChange={handleChange("completed")}
+          type="checkbox"
+          name="completed"
+          id="completed"
+        />
+      </div>
+      <div className="input-control toggler">
+        <label htmlFor="important">Toggle Important</label>
+        <input
+          value={important.toString()}
+          onChange={handleChange("important")}
+          type="checkbox"
+          name="important"
+          id="important"
+        />
+      </div>
     </div>
   );
 }
