0f6003c7e867ec41f8b5537191a751db4454ce63
HassanMTS <150341521+HassanMTS@users.noreply.github.com>
2024-01-08 22:03:28 +0000

    Changes

--- app/providers/GlobalStyleProvider.tsx
+++ app/providers/GlobalStyleProvider.tsx
@@ -1,13 +1,13 @@
-'use client';
-import styled from 'styled-components';
-import React from 'react'
+"use client";
+import React from "react";
+import styled from "styled-components";
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
-function GlobalStyleProvider({children}: Props) {
-    return <GlobalStyles>{children}</GlobalStyles>;
+function GlobalStyleProvider({ children }: Props) {
+  return <GlobalStyles>{children}</GlobalStyles>;
 }
 
 const GlobalStyles = styled.div`
@@ -15,11 +15,17 @@ const GlobalStyles = styled.div`
   display: flex;
   gap: 2.5rem;
   height: 100%;
+  transition: all 0.3s ease-in-out;
 
-  .grid{
+  @media screen and (max-width: 768px) {
+    padding: 1rem;
+    gap: 1rem;
+  }
+
+  .grid {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    gap: 1.5
+    gap: 1.5rem;
   }
 `;
 
