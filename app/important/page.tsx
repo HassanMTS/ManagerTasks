769b63fdce8e4a41620c2981538b117e8be1c81a
HassanMTS <150341521+HassanMTS@users.noreply.github.com>
2024-01-11 14:49:15 +0000

    Important

--- app/important/page.tsx
+++ app/important/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from "react";
 import { useGlobalState } from "../context/globalProvider";
-
+import Tasks from "../Components/Tasks/Tasks";
 
 function page() {
     return <div>important</div>
